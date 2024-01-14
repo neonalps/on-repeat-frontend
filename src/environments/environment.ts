c38f0c44b8901c0f652873fcd409cfeae2e39cb2
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2024-01-14 21:25:03 +0100

    chore: use correct prod config (#9)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "http://localhost:3003",
+    apiBaseUrl: "https://api.on-repeat.neonalps.at",
     oauth: {
         spotify: {
             authorizeUrl: "https://accounts.spotify.com/authorize",
             clientId: "fb41df894f1444d79b40f7dacf0e21d3",
-            redirectUri: "http://localhost:4200/oauth/spotify",
+            redirectUri: "https://on-repeat.neonalps.at/oauth/spotify",
         },
     },
 };
