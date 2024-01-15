62e3ad955bfbdb854f8d14320a4ed289e80e52db
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2024-01-15 20:08:01 +0100

    feat: improve responsive design (#11)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { NavComponent } from '@src/app/nav/nav.component';
 import { AuthService } from '@src/app/auth/auth.service';
 import { ModalComponent } from '@src/app/modal/modal/modal.component';
 import { ModalService } from './modal/modal.service';
+import { AppState } from './store.index';
+import { Store } from '@ngrx/store';
+import { selectMenuVisible } from './ui-state/store/ui-state.selectors';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -21,8 +25,22 @@ import { ModalService } from './modal/modal.service';
 export class AppComponent {
   title = 'on-repeat-frontend';
 
-  constructor(private readonly authService: AuthService, private readonly modalService: ModalService) {
+  private menuActive: boolean = false;
+
+  constructor(
+    private readonly authService: AuthService, 
+    private readonly modalService: ModalService,
+    private readonly store: Store<AppState>,
+  ) {
     this.authService.init();
+
+    this.store.select(selectMenuVisible)
+      .pipe(takeUntilDestroyed())
+      .subscribe(menuActive => this.menuActive = menuActive);
+  }
+
+  isMenuActive(): boolean {
+    return this.menuActive;
   }
 
   isModalActive(): boolean {
