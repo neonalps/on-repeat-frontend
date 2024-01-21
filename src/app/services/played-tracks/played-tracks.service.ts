5ab93523b0e0cd231dc04c6534ca976bb083cb96
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2024-01-21 19:42:17 +0100

    Release/v1.4.0 (#22)
    * feat: refactor and improve played tracks UX (#21)
    
    * chore: bump version to v1.4.0

---
+++ src/app/services/played-tracks/played-tracks.service.ts
@@ -0,0 +1,74 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@src/environments/environment';
+import { PaginatedResponseDto, PlayedTrackApiDto } from '@src/app/models';
+import { Observable, of, switchMap, take } from 'rxjs';
+import { API_QUERY_PARAM_NEXT_PAGE_KEY, hasText, isDefined } from '@src/app/util/common';
+import { getGroupableDateString } from '@src/app/util/date';
+import { groupBy } from '@src/app/util/collection';
+
+export interface PlayedTracksOnDate {
+  date: string;
+  tracks: PlayedTrackApiDto[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayedTracksService {
+
+  private static readonly PLAYED_TRACKS_URL =`${environment.apiBaseUrl}/api/v1/played-tracks`;
+
+  private nextPageKey: string | null = null;
+  private playedTracksOnDate: PlayedTracksOnDate[] = [];
+
+  constructor(private readonly http: HttpClient) {}
+
+  getRecentlyPlayedTracks(): PlayedTracksOnDate[] {
+    return this.playedTracksOnDate;
+  }
+
+  loadRecentlyPlayedTracks(): Observable<PlayedTracksOnDate[]> {
+    return this.fetchRecentlyPlayedTracks()
+      .pipe(
+        take(1),
+        switchMap((response: PaginatedResponseDto<PlayedTrackApiDto>) => {
+          this.processIncomingPlayedTracks(response.items);
+          this.nextPageKey = hasText(response.nextPageKey) ? response.nextPageKey as string : null;
+          return of(this.playedTracksOnDate);
+        })
+      );
+  }
+
+  areMoreRecentlyPlayedTracksAvailable(): boolean {
+    return hasText(this.nextPageKey);
+  }
+
+  private fetchRecentlyPlayedTracks(): Observable<PaginatedResponseDto<PlayedTrackApiDto>> {
+    const queryParams = new URLSearchParams();
+    if (isDefined(this.nextPageKey)) {
+      queryParams.set(API_QUERY_PARAM_NEXT_PAGE_KEY, this.nextPageKey as string);
+    }
+
+    const requestUrl = `${PlayedTracksService.PLAYED_TRACKS_URL}?${queryParams.toString()}`;
+    return this.http.get<PaginatedResponseDto<PlayedTrackApiDto>>(requestUrl);
+  }
+
+  private processIncomingPlayedTracks(items: PlayedTrackApiDto[]): void {
+    const groupedTracks = groupBy(items, (track: PlayedTrackApiDto) => getGroupableDateString(new Date(track.playedAt)));
+
+    for (const [date, tracks] of groupedTracks) {
+      const currentTracksOnDate = this.playedTracksOnDate.find(item => item.date === date);
+
+      if (isDefined(currentTracksOnDate)) {
+        (currentTracksOnDate as PlayedTracksOnDate).tracks.push(...tracks);
+      } else {
+        this.playedTracksOnDate.push({
+          date,
+          tracks
+        });
+      }
+    }
+  }
+
+}
