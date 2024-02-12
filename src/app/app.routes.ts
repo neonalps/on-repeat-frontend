0367a02177538a27ac9b052e64801428749d353b
Michael Stifter <michael@neonalps.at>
2024-02-12 21:27:37 +0100

    feat: add chart types to period charts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { loggedInGuard } from '@src/app/auth/guards/loggedin.guard';
 import { RecentlyPlayedComponent } from '@src/app/recently-played/recently-played.component';
 import { ProfileComponent } from '@src/app/pages/profile/profile.component';
 import { TrackDetailsComponent } from '@src/app/pages/track-details/track-details.component';
-import { PATH_PARAM_ARTIST_SLUG, PATH_PARAM_CHART_PERIOD_SLUG, PATH_PARAM_CHART_SLUG, PATH_PARAM_TRACK_SLUG } from '@src/app/util/router';
+import { PATH_PARAM_ARTIST_SLUG, PATH_PARAM_CHART_PERIOD_SLUG, PATH_PARAM_CHART_SLUG, PATH_PARAM_CHART_TYPE, PATH_PARAM_TRACK_SLUG } from '@src/app/util/router';
 import { ArtistDetailsComponent } from '@src/app/pages/artist-details/artist-details.component';
 import { ChartComponent } from '@src/app/pages/chart/chart.component';
 import { ChartDetailsComponent } from '@src/app/pages/chart-details/chart-details.component';
@@ -24,7 +24,7 @@ export const routes: Routes = [
         canActivate: [loggedInGuard],
     },
     { 
-        path: `charts/period/:${PATH_PARAM_CHART_PERIOD_SLUG}`, 
+        path: `charts/period/:${PATH_PARAM_CHART_TYPE}/:${PATH_PARAM_CHART_PERIOD_SLUG}`, 
         component: ChartPeriodComponent,
         canActivate: [loggedInGuard],
     },
